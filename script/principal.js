88ab93d94875996022e36fc13fd9dd9ae1ae3768
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2023-10-02 11:34:24 -0300

    Add files via upload

--- script/principal.js
+++ script/principal.js
@@ -17,16 +17,40 @@ let b = "";
 let op = "";
 let valor = "";
 let tem_ponto = false;
+let desligada = false;
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    op = "";
+    valor = "";
+    tem_ponto = false;
+    mostra_resultado(0);
+}
+function desligar(){
+    if(desligada){
+        desligada = false;
+        zerar();
+    }else{
+        zerar();
+        mostra_resultado("");
+        desligada = true;
+    }
+    return desligada;
+}
 function mostra_resultado(resul){
+    if(desligada) return;
      document.getElementById("resultado").value = resul;
 }
 function operacao(nova_op){
+    if(desligada) return;
     op = nova_op;
     a = valor;
     valor = "";
     tem_ponto = false;
 }
 function calcula(){
+    if(desligada) return;
     if(op != ""){
         b = valor;
         valor = "";
@@ -41,6 +65,7 @@ function calcula(){
     }
 }
 function digitando(tecla){
+    if(desligada) return;
     if(tecla == "."){
          if(!tem_ponto){
             valor = valor + tecla;
